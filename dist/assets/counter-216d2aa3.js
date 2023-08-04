f5ab6105d8b90c9d9dbd118420d1c2bfdbc017fb
betir <betirov95@gmail.com>
2023-08-05 00:59:29 +0300

    deploy

---
+++ dist/assets/counter-216d2aa3.js
@@ -0,0 +1,1 @@
+import{m as o,j as e}from"./index-b0548f3b.js";const a=o("parent",()=>{const t=e(""),r=e(""),n=e([]);return{name:t,age:r,items:n}});export{a as p};
---
+++ dist/ic/Icons.js
@@ -0,0 +1,9 @@
+
+export class Icons{
+
+    static BASE_PUBLIC_PATH = ".ic/"
+
+    static DEFAULT = ""
+    static  ADD_ICON = Icons.BASE_PUBLIC_PATH + "add_ic_r.svg"
+}
+
